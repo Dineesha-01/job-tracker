a5d503616251189eb79bdc99bfeb998ff5c1516e
Bus Tracking Developer <developer@bustracking.com>
2026-07-04 18:57:31 +0530

    Fix middleware path

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Job = require('../models/Job')
-const protect = require('../middleware/authMiddleware')
-
+const protect = require('../../server/middleware/authMiddleware')
 // GET all jobs for logged in user
 router.get('/', protect, async (req, res) => {
   try {
